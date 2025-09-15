888b626d5f7adc8fa06d1d61e83e82b6c4f1c19b
Dhaval Patil <dhavalpatil2003@gmail.com>
2025-09-15 23:17:39 +0530

    first commit

--- src/components/LoadingSpinner.jsx
+++ src/components/LoadingSpinner.jsx
@@ -1,12 +1,18 @@
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 const LoadingSpinner = () => {
   return (
+    <>
+    <Navbar/>
     <div className="min-h-screen bg-gray-50 flex items-center justify-center">
       <div className="text-center">
         <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-orange-500 mx-auto"></div>
         <p className="mt-4 text-slate-600">Loading products...</p>
       </div>
     </div>
+    <Footer/>
+    </>
   );
 };
 
