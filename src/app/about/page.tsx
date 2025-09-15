ac68b9955ebd0384a1ce32395f82e103db3dbbc6
Dhaval Patil <dhavalpatil2003@gmail.com>
2025-09-15 11:55:20 +0530

    first commit

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,14 +1,14 @@
-// import Navbar from "../../components/Navbar"
-// import Footer from "../../components/Footer"
-// import About from "../../pages/About/AboutUs"
+import Navbar from "../../components/Navbar"
+import Footer from "../../components/Footer"
+import About from "../../pages/About/AboutUs"
 
 
-// export default function Home() {
-//   return (
-//     <>
-//   <Navbar/>
-//   <About/>
-//   <Footer/>
-//     </>
-//   );
-// }
+export default function Home() {
+  return (
+    <>
+  <Navbar/>
+  <About/>
+  <Footer/>
+    </>
+  );
+}
